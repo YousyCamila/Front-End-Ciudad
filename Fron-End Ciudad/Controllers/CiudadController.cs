c6265518d802581f6221e844761b671c5db8eb53
Camila Guerra <danna_cguerra@soy.sena.edu.co>
2024-08-20 20:49:12 -0500

    feat(back-end): Add CiudadController to Controllers

---
+++ Fron-End Ciudad/Controllers/CiudadController.cs
@@ -0,0 +1,43 @@
+﻿using Fron_End_Ciudad.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Fron_End_Ciudad.Controllers
+{
+    public class CiudadController : Controller
+    {
+       
+        public IActionResult Crear()
+        {
+           
+            ViewBag.Departamentos = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Departamento 1" },
+            new SelectListItem { Value = "2", Text = "Departamento 2" },
+            new SelectListItem { Value = "3", Text = "Departamento 3" }
+        };
+
+            return View();
+        }
+
+       
+        [HttpPost]
+        public IActionResult Crear(Ciudad ciudad)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Mostrar", ciudad);
+            }
+
+           
+            ViewBag.Departamentos = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Departamento 1" },
+            new SelectListItem { Value = "2", Text = "Departamento 2" },
+            new SelectListItem { Value = "3", Text = "Departamento 3" }
+        };
+            return View(ciudad);
+        }
+    }
+
+}
